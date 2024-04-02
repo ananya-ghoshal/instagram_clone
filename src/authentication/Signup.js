cc151a9565ef7fc69aa581e408c91f23f9308f25
Ananya Ghoshal <ananyaghoshal@Ananyas-MacBook-Air.local>
2024-04-02 12:07:02 +0530

    login page,sidenav added

---
+++ src/authentication/Signup.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import './Signup.css'
+
+function Signup() {
+    const[email,setEmail]=useState("");
+    const[username,setUsername]=useState("");
+    const[password,setPassword]=useState("");
+  return (
+    <div className="signup">
+      <img src="instagram_texticon.png" alt="" />
+      <input onChange = {e => setEmail(e.target.value)} type="email" placeholder="Email" value={email}/>
+      <input onChange = {e => setUsername(e.target.value)} type="text" placeholder="Username" value={username}/>
+      <input onChange = {e => setPassword(e.target.value)} type="password" placeholder="Password" value={password}/>
+      <button>Signup</button>
+    </div>
+  )
+}
+
+export default Signup
---
+++ src/navigation/Sidenav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Sidenav.css";
+
+function Sidenav(){
+    return(
+        <div className="sidenav"></div>
+    )
+}
+export default Sidenav;
